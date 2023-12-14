0f461149d6284d6d483788f7cb7a8777ff4596c7
Udayv1990 <udayv1990@gmail.com>
2023-12-14 18:20:58 +0530

    Update LoginTests.java

--- src/test/java/com/pocabc/LoginTests.java
+++ src/test/java/com/pocabc/LoginTests.java
@@ -3,6 +3,7 @@ package com.pocabc;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
